db4e242f6a5b8762ef6d35db1b1968acff413cf6
82104 <82104@DESKTOP-V1IEGAQ>
2023-03-16 16:14:00 +0900

    assignment1_3

--- xmanage/src/xmanage/Food.java
+++ xmanage/src/xmanage/Food.java
@@ -1,5 +1,13 @@
 package xmanage;
 
 public class Food {
+	public static void main(String args[]) {
+		System.out.println("1. Add food");
+		System.out.println("2. Delete food");
+		System.out.println("3. Edit food");
+		System.out.println("4. View food");
+		System.out.println("5. Exit");
+		
+	}
 
 }
